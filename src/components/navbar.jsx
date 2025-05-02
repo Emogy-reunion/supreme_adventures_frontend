81e8b38d4e6d3eb5c8632066419bc861a2973b6e
Emogy-reunion <mv7786986@gmail.com>
2025-05-03 02:44:17 +0300

    fix: fix the typo error in the importation statement

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-mport React, { useState } from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from '../styles/Navbar.module.css';
