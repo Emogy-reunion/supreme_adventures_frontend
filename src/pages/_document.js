9a4b4009890740c59e42edc790df7ac3d60d53b1
Emogy-reunion <mv7786986@gmail.com>
2025-05-02 12:55:05 +0300

    modify: modify the meta tags to contain info about my application

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,12 +2,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+	  <Html lang="en">
+	  <Head>
+		<meta name="description" content="Supreme Adventures is a travel booking web app for discovering and reserving tours. Explore top destinations, curated adventures, and seamless bookings—all in one place. Perfect for solo, group, or family trips." />
+	  	<meta name="keywords" content="tour booking, travel app, adventure tours, vacation packages, group travel, solo travel, family trips, travel experiences, book tours online, travel destinations
+" />
+	  	<meta name="author" content="Supreme Adventures" />
+	  	<meta charSet="UTF-8" />
+	  </Head>
+	  <body>
+	  	<Main />
+	  	<NextScript />
+	  </body>
+	  </Html>
   );
 }
