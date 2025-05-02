cc35a3ed7af1b388d3b7e7d3050e20838bac3acb
Emogy-reunion <mv7786986@gmail.com>
2025-05-02 17:40:58 +0300

    refactor: use react class naming syntax and apply them accordingly

--- src/components/landinghero.jsx
+++ src/components/landinghero.jsx
@@ -1,14 +1,15 @@
 import react from 'react';
+import styles from '../styles/Hero.module.css';
 
 
 const LandingHero = () => {
         return (
                 <>
-                        <section class="hero">
-                                <div class="hero-content">
+                        <section className={styles.hero}>
+                                <div className={styles['hero-content']}>
                                         <h1>Discover Your Next Adventure</h1>
                                         <p>Tailored tours to the world's most stunning destinations</p>
-                                        <a href="#tours"><button class="btn">Explore Tours</button></a>
+                                        <a href="#tours"><button className={styles.btn}>Explore Tours</button></a>
                                 </div>
                         </section>
                 </>
