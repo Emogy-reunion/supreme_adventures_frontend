882083b245eb2a0466649db77acb1b8fdc364d4c
Emogy-reunion <mv7786986@gmail.com>
2025-05-02 17:08:39 +0300

    add: create a component that creates the landing page

---
+++ src/components/landinghero.jsx
@@ -0,0 +1,18 @@
+import react from 'react';
+
+
+const LandingHero = () => {
+        return (
+                <>
+                        <section class="hero">
+                                <div class="hero-content">
+                                        <h1>Discover Your Next Adventure</h1>
+                                        <p>Tailored tours to the world's most stunning destinations</p>
+                                        <a href="#tours"><button class="btn">Explore Tours</button></a>
+                                </div>
+                        </section>
+                </>
+        );
+};
+
+export default LandingHero;
