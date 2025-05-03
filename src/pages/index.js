816f1c684603db6611cd99c9caaa6b1817c7978f
Emogy-reunion <mv7786986@gmail.com>
2025-05-03 18:40:11 +0300

    initialize the popular destinations component with the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import NavBar from '../components/Navbar';
+import NavBar from '../components/navbar';
+import PopularDestinations form '../components/populardestinations';
 
 const Home = () => {
 	return (
 		<>
 			<NavBar />
+			<PopularDestinations />
 		</>
 	);
 };
