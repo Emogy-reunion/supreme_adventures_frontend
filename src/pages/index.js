fcd4f0772acbd646c792884e0eafe5ab55ed57c2
Emogy-reunion <mv7786986@gmail.com>
2025-05-03 19:09:22 +0300

    fix: import the landing hero component correctly

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from '../components/navbar';
-import LandingHero from '.../components/landinghero';
+import LandingHero from '../components/landinghero';
 import PopularDestinations from '../components/populardestinations';
 
 const Home = () => {
