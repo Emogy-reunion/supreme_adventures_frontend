3eb0ec46fb734a95676bb4f872417e6870ba091d
Emogy-reunion <mv7786986@gmail.com>
2025-05-04 20:46:58 +0300

    add: add a component for the why choose us section

---
+++ src/components/whychooseuse.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styles from '../styles/Whychooseus.module.css';
+
+
+const WhyChooseUs = () => {
+	return (
+		<>
+			<section>
+				<h2>Why Choose Supreme Adventures?</h2>
+				
+				<div className={styles["icon-grid"]}>
+      					<div className={styles["icon-box"]}>
+        					<img src="https://img.icons8.com/ios-filled/50/globe--v1.png" alt="Global">
+        					<h3>Expert Guides</h3>
+        					<p>Travel with experienced professionals who know the hidden gems.</p>
+      					</div>
+
+      					<div className={styles["icon-box"]}>
+        					<img src="https://img.icons8.com/ios-filled/50/discount.png" alt="Price">
+        					<h3>Best Prices</h3>
+        					<p>Affordable and transparent pricing. No surprises.</p>
+      					</div>
+
+      					<div className={styles["icon-box"]}>
+        					<img src="https://img.icons8.com/ios-filled/50/phone.png" alt="Support">
+        					<h3>24/7 Support</h3>
+        					<p>We’re here anytime you need us, wherever you are.</p>
+      					</div>
+    				</div>
+  			</section>
+		</>
+	);
+};
+
+
+export default WhyChooseUs;
