e6d62ead009cdde717e11b885f1dc72891f114cf
Emogy-reunion <mv7786986@gmail.com>
2025-05-05 13:13:32 +0300

    refactor: remove the br tags

--- src/components/reviewspreview.jsx
+++ src/components/reviewspreview.jsx
@@ -9,19 +9,16 @@ const ReviewsPreview = () => {
     				<div className={styles["testomonials-container"]}>
       					<div className={styles.testimonial}>
         					<p>“Our Dubai experience was incredible thanks to the amazing organization and attention to detail. Everything ran smoothly from start to finish!.”</p>
-        					<br><br/>
 						<h4>- Shantel.</h4>
       					</div>
       					
 					<div className={styles.testimonial}>
         					<p>“Seeing lions up close on the safari was an unforgettable moment. Highly recommend!”</p>
-        					<br><br/>
 						<h4>- Omosh.</h4>
       					</div>
 
       					<div className={styles.testimonial}>
         					<p>“The Singapore tour was perfectly planned and stress-free. The guides were professional, friendly, and made sure we saw the best of the city!”</p>
-        					<br><br/>
 						<h4>- Mark.</h4>
       					</div>
     				</div>
