0e74f89db0d149bf8edcb69fb2eb846055ba012f
Emogy-reunion <mv7786986@gmail.com>
2025-05-05 16:58:10 +0300

    add: create the footer for the page"

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Link from 'next/link';
+import styles from '../styles/Footer.module.css';
+
+
+const Footer = () => {
+	return (
+		<>
+			<footer className={styles.footer}>
+  				<p>&copy; 2025 Supreme Adventures. All Rights Reserved.</p>
+    				<p>		
+					<Link href="/" className={styles.link}>Home</Link> |{" "}
+    					<Link href="#" className={styles.link}>Tours</Link> |{" "}
+    					<Link href="#" className={styles.link}>Contact</Link>
+  				</p>
+			</footer>
+		</>
+	);
+};
+
+
+export default Footer;
+			
