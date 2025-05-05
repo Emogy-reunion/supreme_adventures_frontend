942a83c35781b00deccf7195b216366f661b0201
Emogy-reunion <mv7786986@gmail.com>
2025-05-05 12:52:00 +0300

    fix: fix the syntax error by correcting how br is used

--- src/components/reviewspreview.jsx
+++ src/components/reviewspreview.jsx
@@ -9,17 +9,20 @@ const ReviewsPreview = () => {
     				<div className={styles["testomonials-container"]}>
       					<div className={styles.testimonial}>
         					<p>“Our Dubai experience was incredible thanks to the amazing organization and attention to detail. Everything ran smoothly from start to finish!.”</p>
-        					<br Maria./>
+        					<br><br/>
+						- Shantel.
       					</div>
       					
 					<div className={styles.testimonial}>
         					<p>“Seeing lions up close on the safari was an unforgettable moment. Highly recommend!”</p>
-        					<br Omosh. />
+        					<br><br/>
+						- Omosh.
       					</div>
 
       					<div className={styles.testimonial}>
         					<p>“The Singapore tour was perfectly planned and stress-free. The guides were professional, friendly, and made sure we saw the best of the city!”</p>
-        					<br Anita R. />
+        					<br><br/>
+						- Mark.
       					</div>
     				</div>
   			</section>
