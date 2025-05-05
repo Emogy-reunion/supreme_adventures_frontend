867e54bf2baec6d73be5ea4a816159e9609fc9fd
Emogy-reunion <mv7786986@gmail.com>
2025-05-05 16:08:55 +0300

    add styles correctly

--- src/components/newsletter.jsx
+++ src/components/newsletter.jsx
@@ -5,12 +5,12 @@ import styles from '../styles/Newsletter.module.css';
 const NewsLetter = () => {
 	return (
 		<>
-		 	<section className="newsletter-section">
+		 	<section className={styles["newsletter-section"]}>
     				<h2>Join Our Newsletter</h2>
-    				<p style="text-align:center;">Get travel inspiration and offers straight to your inbox.</p>
+    				<p>Get travel inspiration and offers straight to your inbox.</p>
     				<form>
       					<input type="email" placeholder="Enter your email" required />
-      					<button className="newsletter-btn" type="submit">Subscribe</button>
+      					<button className={styles["newsletter-btn"]} type="submit">Subscribe</button>
     				</form>
   			</section>
 		</>
