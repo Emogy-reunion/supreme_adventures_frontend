80c866119a57d61ca1f20bb4d427e4577e77a68b
Emogy-reunion <mv7786986@gmail.com>
2025-05-06 12:25:22 +0300

    create: create the login page and add the necessary components

---
+++ src/pages/login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+import LoginForm from '../components/loginform';
+import Footer from '../components/footer';
+
+
+const Login = () => {
+	return (
+		<>
+			<Navbar />
+			<LoginForm />
+			<Footer/>
+		</>
+	);
+};
+
+
+export default Login;
