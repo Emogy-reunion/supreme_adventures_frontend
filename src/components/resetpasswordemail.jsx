cc03099b1ed6ab82824df72014bcdbe84f8c848a
Emogy-reunion <mv7786986@gmail.com>
2025-05-08 11:05:02 +0300

    fix: import link without the leading backslash

--- src/components/resetpasswordemail.jsx
+++ src/components/resetpasswordemail.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Link from '/next/link';
+import Link from 'next/link';
 import styles from '../styles/Resetpasswordemail.module.css';
 
 const ResetPasswordEmail = () => {
