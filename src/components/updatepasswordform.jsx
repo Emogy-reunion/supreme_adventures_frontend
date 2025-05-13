258a4dd470f00390669c084406de13351d06b3ab
Emogy-reunion <mv7786986@gmail.com>
2025-05-13 13:24:57 +0300

    fix: correct the path to the  css file to fix the module not found error, import the Link component since it is used for routing

--- src/components/updatepasswordform.jsx
+++ src/components/updatepasswordform.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import styles from '../components/Updatepasswordform.module.css';
+import Link from 'next/link';
+import styles from '../src/Updatepasswordform.module.css';
 
 
 const UpdatePasswordForm = ({ userId }) => {
