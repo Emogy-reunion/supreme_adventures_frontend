14cc270c3ece427d7be2513c6ca58b91dae52f42
Emogy-reunion <mv7786986@gmail.com>
2025-05-13 13:26:26 +0300

    fix: correct the path to the  css file to fix the module not found errorwq

--- src/components/updatepasswordform.jsx
+++ src/components/updatepasswordform.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from '../src/Updatepasswordform.module.css';
+import styles from '../styles/Updatepasswordform.module.css';
 
 
 const UpdatePasswordForm = ({ userId }) => {
