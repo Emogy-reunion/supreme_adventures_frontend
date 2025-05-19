bdfa87fabb38b3987380e73330c6ce396d09ee5d
Emogy-reunion <mv7786986@gmail.com>
2025-05-19 15:32:29 +0300

    fix: fix the reference error by importing useState

--- src/components/updatepasswordform.jsx
+++ src/components/updatepasswordform.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/Updatepasswordform.module.css';
+import { useState } from 'react';
 
 
 const UpdatePasswordForm = ({ token }) => {
