01ed26324338e94868031a9fb4235ad8aeeeaa84
Emogy-reunion <mv7786986@gmail.com>
2025-06-09 12:30:04 +0300

    import the correct component

--- src/pages/destinations.js
+++ src/pages/destinations.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import NavBar from '../components/navbar.jsx';
 import Footer from '../components/footer.jsx';
-import LocalPackagesSwiper from '../components/localpackages.jsx';
+import PackagesSwiper from '../components/localpackages.jsx';
 
 
 const Destinations = () => {
 	return (
 		<>
 			<NavBar />
-			<LocalPackagesSwiper />
+			<PackagesSwiper />
 			<Footer />
 		</>
 	);
