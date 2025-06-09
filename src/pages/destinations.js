601d8657ccdb597288d98fa69900874b3ed6fb08
Emogy-reunion <mv7786986@gmail.com>
2025-06-09 11:35:07 +0300

    create: create the destination page that will store tour destinations

---
+++ src/pages/destinations.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from '../components/navbar.jsx';
+import Footer from '../components/footer.jsx';
+import LocalPackagesSwiper from '../components/localpackages.jsx';
+
+
+const Destinations = () => {
+	return (
+		<>
+			<NavBar />
+			<LocalPackagesSwiper />
+			<Footer />
+		</>
+	);
+};
+
+
+export default Destinations;
