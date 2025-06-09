5e19785607c83c2214d1233f2097bd49be9cb776
Emogy-reunion <mv7786986@gmail.com>
2025-06-09 19:26:02 +0300

    add: create the contact page that allows users to reach out to admins

---
+++ src/pages/contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../components/navbar';
+import Footer from '../components/footer';
+import ContactSection from '../components/contactsection.jsx';
+
+const Contact = () => {
+        return (
+                <>
+                        <NavBar />
+                        <ContactSection />
+                        <Footer />
+                </>
+        );
+};
+
+export default Contact;
