294a7fdb96164d3044d8e8d110beefa5de0cdfea
Emogy-reunion <mv7786986@gmail.com>
2025-06-17 11:04:32 +0300

    fix: create a page that will be used to display destinations, it is wrapped using a withAuth to ensure only logged in users can access it

---
+++ src/pages/member-destinations.js
@@ -0,0 +1,17 @@
+mport React from 'react';
+import NavBar from '../components/membernavbar';
+import PackagesSwiper from '../components/localpackages';
+import withAuth from '../hoc/withAuth';
+
+
+const Destinations = () => {
+        return (
+                <>
+                        <MemberNavBar />
+                        <PackagesSwiper />
+                </>
+        );
+};
+
+
+export default withAuth(MemberDestinations);
