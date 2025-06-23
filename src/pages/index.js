f4525b629aa13cab7127a0040343c69f96453cf4
Emogy-reunion <mv7786986@gmail.com>
2025-06-23 14:29:43 +0300

    add a discover section to the guest and member users dashboards

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import LandingHero from '../components/landinghero';
 import PopularDestinations from '../components/populardestinations';
 import WhyChooseUs from '../components/whychooseus';
 import ReviewsPreview from '../components/reviewspreview.jsx';
-import NewsLetter from '../components/newsletter.jsx';
+import DiscoverSection from '../components/discover.jsx';
 import Footer from '../components/footer.jsx';
 
 const Home = () => {
@@ -15,7 +15,7 @@ const Home = () => {
 			<PopularDestinations />
 			<WhyChooseUs />
 			<ReviewsPreview />
-			<NewsLetter />
+			<DiscoverSection />
 			<Footer />
 		</>
 	);
