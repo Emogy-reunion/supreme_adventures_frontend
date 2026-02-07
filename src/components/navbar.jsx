345c16ec8ac2d7d57f9aed218b4672f9af4d1742
Emogy-reunion <mv7786986@gmail.com>
2026-02-07 12:56:50 +0300

    refactor: convert the component to a client component since in app router all components are server side

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,8 @@
+'use client';
+
 import React, { useState } from 'react';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import styles from '../styles/Navbar.module.css';
 
 const NavBar = () => {
