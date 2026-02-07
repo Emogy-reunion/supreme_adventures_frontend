2cdccc23f7f5918f3f2f03df76f7c5f49efbef4f
Emogy-reunion <mv7786986@gmail.com>
2026-02-07 16:20:45 +0300

    add: create a folder that will house the guest dashboard route, add the about page

---
+++ src/app/guest/about/page.js
@@ -0,0 +1,15 @@
+import NavBar from '@/components/navbar';
+import Footer from '@/components/footer';
+import AboutUs from '@/components/aboutus';
+
+const About = () => {
+        return (
+                <>
+                        <NavBar />
+                        <AboutUs />
+                        <Footer />
+                </>
+        );
+};
+
+export default About;
