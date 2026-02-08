3d9949de3f1e5e148b10cca635b784b6e03cb2d9
Emogy-reunion <mv7786986@gmail.com>
2026-02-08 19:04:48 +0300

    add: create a file to store configuration settings of where requests will be proxied to

---
+++ next.config.js
@@ -0,0 +1,35 @@
+/** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      // Development (local Flask)
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '5000',
+        pathname: '/api/send_image/**',
+      },
+
+      // Production (Flask behind Nginx)
+      {
+        protocol: 'https',
+        hostname: 'supremeadventures.co.ke',
+        pathname: '/api/send_image/**',
+      },
+    ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.BACKEND_URL}/api/:path*`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig
